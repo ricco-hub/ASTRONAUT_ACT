4a54c7474717b84a9a831233fcace5e7b2c27e9b
ricco-hub <rcv38@cornell.edu>
2026-07-25 16:46:41 -0400

    Add inverse var ftns

--- ast_ftns.py
+++ ast_ftns.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from astropy.io import fits
 
 MODULE_DIR = Path(__file__).resolve().parent
 FILE_PATH = MODULE_DIR / "asteroid_list.txt"
@@ -70,3 +71,41 @@ def find_freq(freq: str) -> None:
                 print(line, end="")
             else:
                 continue
+
+
+def inv_var(data: list[float], variances: list[float]) -> tuple[float, float]:
+    """
+    Inputs: data, list of data 
+            variances, list of variances associated with data
+    Outputs: inverse variance weighted average and inverse variance
+    """
+
+    ave = 0
+    var = 0
+    for i in range(len(data)):
+        ave += data[i]/variances[i]
+        var += 1/variances[i]
+    return ave/var, 1/var
+
+
+def get_ast_weighted_flux(ast: str, array: str, freq: str) -> tuple[float, float]:
+    """
+    Inputs: ast, name of asteroid
+            array, ACT array: pa4, pa5, pa6
+            freq, frequency (GHz): 090, 150, 220
+    Outputs: weight_flux, inverse variance weighted flux
+             weight_var, inverse variance
+    """
+
+    name = f'{ast}_lc_{array}_{freq}'
+
+    s3_path = 's3://cornell-acteroids/' + name + '.fits'
+    with fits.open(s3_path, fsspec_kwargs={"anon": True}) as hdul:  
+        data = hdul[1].data
+    # get data from each column
+    flux = data['Flux']
+    error = data['FluxUncertainty']
+    weight = data['Weight']
+    weight_flux, weight_var = inv_var(flux*weight, (error*weight)**2) # Inverse variance weighted ACT flux scaled to 1au/1au/alpha0
+
+    return weight_flux, weight_var
